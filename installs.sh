89a7ee910fe3154e6eeff92af30c3cecdbbcdf29
Blins413 <johnharding413@gmail.com>
2026-06-18 15:13:29 +0000

    Update Thu 18 Jun 2026 15:13:28 UTC

--- installs.sh
+++ installs.sh
@@ -7,10 +7,21 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+yay -Syu
 
 ## Installing Essentials/Tools
 sudo pacman -S neovim
 sudo pacman -S yadm
+sudo pacman -S quickshell
+
+
+
+## Installing Environment Apps
+
+##Wallpaper Picker
+sudo pacman -S awww
+sudo pacman -S imagemagick
+sudo pacman -S jq
 
 ## Installing Apps
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
@@ -18,3 +29,6 @@ git clone https://aur.archlinux.org/1password.git
 cd 1password/
 makepkg -si
 cd
+
+##Notify of Manual Adjustments
+echo "~/.config/quickshell/hyprquickpaper/config.json will need adjusting to the appropriate user"
